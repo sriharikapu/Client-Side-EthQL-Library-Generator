97f60b8b3046c503c87cddcff7972d44a73baf23
Ryan Christoffersen <ryan@ryanchristo.com>
2019-02-16 11:25:58 -0700

    Move contract build files and add to cli

--- cli/index.js
+++ cli/index.js
@@ -2,6 +2,14 @@
 
 const chalk = require('chalk');
 
+const SimpleStorage = require('../contracts/build/SimpleStorage.json');
+
 console.log();
-console.log(`${chalk.cyan('Building JS library...')}`);
+console.log(`${chalk.cyan('Loading contract data...')}`);
+console.log();
+console.log(`${SimpleStorage.contractName}`);
+console.log();
+console.log(`${JSON.stringify(SimpleStorage.abi)}`);
+console.log();
+console.log(`${chalk.cyan('Building JavaScript library...')}`);
 console.log();
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Query } from 'react-apollo';
 
-import SimpleStorageContract from '../../contracts/SimpleStorage.json';
 import getWeb3 from '../../utils/getWeb3';
 import { BLOCK_TEST } from '../../queries';
 
@@ -16,17 +15,9 @@ class App extends Component {
       // Use web3 to get the user's accounts.
       const accounts = await web3.eth.getAccounts();
 
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
-
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance });
+      this.setState({ web3, accounts });
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
