0a784d06e84cb3f67d9943e978a03e656b3f76ad
Ryan Christoffersen <ryan@ryanchristo.com>
2019-02-16 12:13:30 -0700

    Add library import and test

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -3,42 +3,57 @@ import { Query } from 'react-apollo';
 
 import getWeb3 from '../../utils/getWeb3';
 import { BLOCK_TEST } from '../../queries';
+import * as library from '../../lib/index';
 
-class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+class Home extends Component {
+
+  state = {
+    web3: null,
+    accounts: null,
+    testing: null,
+  };
 
   componentDidMount = async () => {
+
     try {
+
       // Get network provider and web3 instance.
       const web3 = await getWeb3();
 
       // Use web3 to get the user's accounts.
       const accounts = await web3.eth.getAccounts();
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts });
+      // Execute library test method.
+      const testing = library.testing();
+
+      // Set web3 and accounts to the state.
+      this.setState({ web3, accounts, testing });
+
     } catch (error) {
+
       // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
+      alert(`Failed to load web3 or accounts. Check console for details.`);
+
+      // Log operation error.
       console.error(error);
+
     }
+
   };
 
   render() {
     if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
+      return <div>Loading Web3 and accounts...</div>;
     }
     return (
       <div>
-        <h1>Test</h1>
+        <h1>Library Test</h1>
+        <p>{this.state.testing}</p>
+        <h1>GraphQL Test</h1>
         <Query query={BLOCK_TEST}>
           {({ loading, error, data }) => {
             if (loading) return <p>Loading...</p>;
             if (error) return <p>Error :(</p>;
-
             return (
               <p>{JSON.stringify(data)}</p>
             );
@@ -49,4 +64,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Home;
